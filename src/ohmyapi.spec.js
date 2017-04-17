a8d4ef0e1629e8a496a881c65fce2e796fab2282
Tan Nguyen <tan.qh.nguyen@gmail.com>
2017-04-17 22:03:22 +0300

    Faster way to test multiple engines

--- src/ohmyapi.spec.js
+++ src/ohmyapi.spec.js
@@ -3,84 +3,90 @@ const ohmyapi = require('./ohmyapi');
 describe('ohmyapi', () => {
   const routes = __dirname + '/test/routes/api';
 
-  describe('express', () => {
-    let app;
+  const engines = [
+    'express'
+  ];
 
-    beforeEach(() => {
-      app = ohmyapi(routes)
-              .engine('express', {
-                prefix: '/api'
-              })
-              .init();
-    });
+  engines.forEach((engine) => {
+    describe(engine, () => {
+      let app;
 
-    it('should return correct format', () => {
-      return callApi(app, {
-        path: '/api/status',
-        method: 'get'
-      }).then((res) => {
-        expect(res.body).to.eql({
-          data: {
-            status: 'ok'
-          },
-          success: true,
-          error: null
-        });
+      beforeEach(() => {
+        app = ohmyapi(routes)
+                .engine(engine, {
+                  prefix: '/api'
+                })
+                .init();
       });
-    });
 
-    it('should handle error', () => {
-      return callApi(app, {
-        path: '/api/users/count',
-        method: 'get'
-      }).then((res) => {
-        expect(res.body).to.eql({
-          data: null,
-          success: false,
-          error: {
-            data: null,
-            message: 'meh'
-          }
-        });
-      });
-    });
-
-    it('should validate input using args option', () => {
-      return callApi(app, {
-        path: '/api/users/1/comments/2/likes',
-        method: 'post'
-      }).then((res) => {
-        expect(res.body).to.eql({
-          data: null,
-          success: false,
-          error: {
-            message: 'Failed to validate request',
+      it('should return correct format', () => {
+        return callApi(app, {
+          path: '/api/status',
+          method: 'get'
+        }).then((res) => {
+          expect(res.body).to.eql({
             data: {
-              content: ['Content can\'t be blank']
-            }
-          }
-        })
-      });
-    });
-
-    it('should filter request args', () => {
-      return callApi(app, {
-        path: '/api/users/1',
-        data: {
-          test: 1
-        },
-        method: 'get'
-      }).then((res) => {
-        expect(res.body).to.eql({
-          data: {
-            id: 1,
-            name: 'meh'
-          },
-          success: true,
-          error: null
+              status: 'ok'
+            },
+            success: true,
+            error: null
+          });
         });
       });
-    });
 
+      it('should handle error', () => {
+        return callApi(app, {
+          path: '/api/users/count',
+          method: 'get'
+        }).then((res) => {
+          expect(res.body).to.eql({
+            data: null,
+            success: false,
+            error: {
+              data: null,
+              message: 'meh'
+            }
+          });
+        });
+      });
+
+      it('should validate input using args option', () => {
+        return callApi(app, {
+          path: '/api/users/1/comments/2/likes',
+          method: 'post'
+        }).then((res) => {
+          expect(res.body).to.eql({
+            data: null,
+            success: false,
+            error: {
+              message: 'Failed to validate request',
+              data: {
+                content: ['Content can\'t be blank']
+              }
+            }
+          })
+        });
+      });
+
+      it('should filter request args', () => {
+        return callApi(app, {
+          path: '/api/users/1',
+          data: {
+            test: 1
+          },
+          method: 'get'
+        }).then((res) => {
+          expect(res.body).to.eql({
+            data: {
+              id: 1,
+              name: 'meh'
+            },
+            success: true,
+            error: null
+          });
+        });
+      });
+
+    });
   });
 });
